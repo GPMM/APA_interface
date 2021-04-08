f9b68fe96ed882d339be456f2c5a0d3f2da1551a
AndrewsRafaela <rafaela.andrews@mobicare.com.br>
2021-04-08 01:27:38 -0300

    ajustes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+/*!
+
+=========================================================
+* Light Bootstrap Dashboard React - v2.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -5,16 +22,16 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/animate.min.css";
-import "./assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0";
+import "./assets/scss/light-bootstrap-dashboard-react.scss?v=2.0.0";
 import "./assets/css/demo.css";
-import "./assets/css/pe-icon-7-stroke.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-import AdminLayout from "layouts/Admin.jsx";
+import AdminLayout from "layouts/Admin.js";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/admin" render={props => <AdminLayout {...props} />} />
+      <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
       <Redirect from="/" to="/admin/dashboard" />
     </Switch>
   </BrowserRouter>,
--- src/routes.js
+++ src/routes.js
@@ -1,38 +1,87 @@
-import Dashboard from "views/Dashboard.jsx";
-import UserProfile from "views/UserProfile.jsx";
-import TableList from "views/TableList.jsx";
-import Typography from "views/Typography.jsx";
-import Notifications from "views/Notifications.jsx";
+/*!
+
+=========================================================
+* Light Bootstrap Dashboard React - v2.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import Dashboard from "views/Dashboard.js";
+import UserProfile from "views/UserProfile.js";
+import TableList from "views/TableList.js";
+import Typography from "views/Typography.js";
+import Icons from "views/Icons.js";
+import Maps from "views/Maps.js";
+import Notifications from "views/Notifications.js";
+import Upgrade from "views/Upgrade.js";
 
 const dashboardRoutes = [
   {
+    upgrade: true,
+    path: "/upgrade",
+    name: "Upgrade to PRO",
+    icon: "nc-icon nc-alien-33",
+    component: Upgrade,
+    layout: "/admin",
+  },
+  {
     path: "/dashboard",
-    name: "Dashboards",
-    icon: "pe-7s-graph",
+    name: "Dashboard",
+    icon: "nc-icon nc-chart-pie-35",
     component: Dashboard,
-    layout: "/admin"
+    layout: "/admin",
   },
   {
     path: "/user",
-    name: "Gestão de usuários",
-    icon: "pe-7s-id",
+    name: "User Profile",
+    icon: "nc-icon nc-circle-09",
     component: UserProfile,
-    layout: "/admin"
+    layout: "/admin",
   },
   {
     path: "/table",
-    name: "Gestão de turmas",
-    icon: "pe-7s-users",
+    name: "Table List",
+    icon: "nc-icon nc-notes",
     component: TableList,
-    layout: "/admin"
+    layout: "/admin",
   },
   {
     path: "/typography",
-    name: "Login",
-    icon: "pe-7s-news-paper",
+    name: "Typography",
+    icon: "nc-icon nc-paper-2",
     component: Typography,
-    layout: "/admin"
-  }
+    layout: "/admin",
+  },
+  {
+    path: "/icons",
+    name: "Icons",
+    icon: "nc-icon nc-atom",
+    component: Icons,
+    layout: "/admin",
+  },
+  {
+    path: "/maps",
+    name: "Maps",
+    icon: "nc-icon nc-pin-3",
+    component: Maps,
+    layout: "/admin",
+  },
+  {
+    path: "/notifications",
+    name: "Notifications",
+    icon: "nc-icon nc-bell-55",
+    component: Notifications,
+    layout: "/admin",
+  },
 ];
 
 export default dashboardRoutes;
